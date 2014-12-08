6f03e1668899e6676599fc4627c037ab2e616cdd
vicionator <oscarin093@hotmail.com>
2014-12-07 20:07:28 -0800

    Refrescar Pantallas

--- Tetris/Tetris/DosJugadoresTablero.cs
+++ Tetris/Tetris/DosJugadoresTablero.cs
@@ -215,11 +215,32 @@ namespace Tetris
                     Socket s1 = myList.AcceptSocket();
                     byte[] b1 = new byte[1000000];
                     int k1 = s.Receive(b1);
+                   
                     MemoryStream memStreamT = new MemoryStream();
                     BinaryFormatter binFormT = new BinaryFormatter();
                     memStreamT.Write(b1, 0, b1.Length);
                     memStreamT.Seek(0, SeekOrigin.Begin);
                     Tablero t = (Tablero)binFormT.Deserialize(memStreamT);
+
+                    byte[] bytesPieza = new byte[1];
+                    IFormatter formatter = new BinaryFormatter();
+                    using (MemoryStream stream = new MemoryStream())
+                    {
+                        formatter.Serialize(stream, pi);
+                        bytesPieza = stream.ToArray();
+                    }
+                    byte[] bytesTab = new byte[1];
+                    IFormatter formattertab = new BinaryFormatter();
+                    using (MemoryStream streamtab = new MemoryStream())
+                    {
+                        formattertab.Serialize(streamtab, tab);
+                        bytesTab = streamtab.ToArray();
+                    }
+
+                    myList.AcceptSocket().Send(bytesPieza);
+                    myList.AcceptSocket().Send(bytesTab);
+
+
 
                     foreach (Cuadro x in t.cuadritos)
                     {
@@ -370,7 +391,6 @@ namespace Tetris
                         final[j] = bytesTab[k];
                         k++;
                     }
-                    final[final.Length] = Convert.ToByte(';');
 
                     NetworkStream cosa = tcpclnt.GetStream();
                     cosa.Write(bytesPieza, 0, bytesPieza.Length);
@@ -378,6 +398,138 @@ namespace Tetris
                     NetworkStream cosa1 = tcpclnt.GetStream();
                     cosa1.Write(bytesTab, 0, bytesTab.Length);
 
+                    byte [] PiezaJ1 = new byte[1];
+
+                    byte[] TabJ1 = new byte[1];
+                   
+                    //Pieza
+                    Stream stm = tcpclnt.GetStream();
+                    byte[] b5 = new byte[1000000];
+                    int k3 = stm.Read(b5, 0, b5.Length);
+
+                    MemoryStream memStream = new MemoryStream();
+                    BinaryFormatter binForm = new BinaryFormatter();
+                    memStream.Write(b5, 0, b5.Length);
+                    memStream.Seek(0, SeekOrigin.Begin);
+                    Pieza p3 = (Pieza)binForm.Deserialize(memStream);
+
+                    //Tablero
+                    Stream stm1 = tcpclnt.GetStream();
+                    byte[] b6 = new byte[1000000];
+                    int k4 = stm.Read(b6, 0, b6.Length);
+
+                    MemoryStream memStreamT = new MemoryStream();
+                    BinaryFormatter binFormT = new BinaryFormatter();
+                    memStreamT.Write(b6, 0, b6.Length);
+                    memStreamT.Seek(0, SeekOrigin.Begin);
+                    Tablero t3 = (Tablero)binFormT.Deserialize(memStreamT);
+
+                    foreach (Cuadro x in t3.cuadritos)
+                    {
+                        x.Brocha = Brocha;
+                        x.Brocha1 = Brocha;
+                        x.Brocha2 = Brocha;
+                        x.Brocha3 = Brocha;
+                        x.Brocha4 = Brocha;
+                        x.Brocha5 = Brocha;
+                        x.Brocha6 = Brocha;
+
+                    }
+
+                    if (p3.pieza.ToString() == "Cubo")
+                    {
+                        foreach (Cuadro c in p3.PiezaO)
+                        {
+                            c.Brocha = Brocha;
+                            c.Brocha1 = Brocha;
+                            c.Brocha2 = Brocha;
+                            c.Brocha3 = Brocha;
+                            c.Brocha4 = Brocha;
+                            c.Brocha5 = Brocha;
+                            c.Brocha6 = Brocha;
+                        }
+                    }
+                    if (p3.pieza.ToString() == "I")
+                    {
+                        foreach (Cuadro c in p3.PiezaO)
+                        {
+                            c.Brocha = Brocha3;
+                            c.Brocha1 = Brocha3;
+                            c.Brocha2 = Brocha3;
+                            c.Brocha3 = Brocha3;
+                            c.Brocha4 = Brocha3;
+                            c.Brocha5 = Brocha3;
+                            c.Brocha6 = Brocha3;
+                        }
+                    }
+                    if (p3.pieza.ToString() == "Jota")
+                    {
+                        foreach (Cuadro c in p3.PiezaO)
+                        {
+                            c.Brocha = Brocha2;
+                            c.Brocha1 = Brocha2;
+                            c.Brocha2 = Brocha2;
+                            c.Brocha3 = Brocha2;
+                            c.Brocha4 = Brocha2;
+                            c.Brocha5 = Brocha2;
+                            c.Brocha6 = Brocha2;
+                        }
+                    }
+                    if (p3.pieza.ToString() == "Te")
+                    {
+                        foreach (Cuadro c in p3.PiezaO)
+                        {
+                            c.Brocha = Brocha6;
+                            c.Brocha1 = Brocha6;
+                            c.Brocha2 = Brocha6;
+                            c.Brocha3 = Brocha6;
+                            c.Brocha4 = Brocha6;
+                            c.Brocha5 = Brocha6;
+                            c.Brocha6 = Brocha6;
+                        }
+                    }
+                    if (p3.pieza.ToString() == "ESE")
+                    {
+                        foreach (Cuadro c in p3.PiezaO)
+                        {
+                            c.Brocha = Brocha4;
+                            c.Brocha1 = Brocha4;
+                            c.Brocha2 = Brocha4;
+                            c.Brocha3 = Brocha4;
+                            c.Brocha4 = Brocha4;
+                            c.Brocha5 = Brocha4;
+                            c.Brocha6 = Brocha4;
+                        }
+                    }
+                    if (p3.pieza.ToString() == "ELE")
+                    {
+                        foreach (Cuadro c in p3.PiezaO)
+                        {
+                            c.Brocha = Brocha1;
+                            c.Brocha1 = Brocha1;
+                            c.Brocha2 = Brocha1;
+                            c.Brocha3 = Brocha1;
+                            c.Brocha4 = Brocha1;
+                            c.Brocha5 = Brocha1;
+                            c.Brocha6 = Brocha1;
+                        }
+                    }
+                    if (p3.pieza.ToString() == "Zeta")
+                    {
+                        foreach (Cuadro c in p3.PiezaO)
+                        {
+                            c.Brocha = Brocha5;
+                            c.Brocha1 = Brocha5;
+                            c.Brocha2 = Brocha5;
+                            c.Brocha3 = Brocha5;
+                            c.Brocha4 = Brocha5;
+                            c.Brocha5 = Brocha5;
+                            c.Brocha6 = Brocha5;
+                        }
+                    }
+                    tab2 = t3;
+                    RefrescarS(p3);
+                    
                     
                 }
                 catch(Exception e)
